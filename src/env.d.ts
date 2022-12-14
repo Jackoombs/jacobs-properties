4ed4685f4b975dc1d05cbed60b404837ef7dd3b0
Jack Coombs <jack.coombs@hotmail.co.uk>
2022-12-14 15:26:48 +0000

    Added a type for properties.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="astro/client" />
+
+export interface Property {
+  ID: string;
+  Description: string;
+  Image: Array<Image>;
+  Address1: string;
+  Address2: string;
+  Status: string;
+  PriceString: string;
+  TotalBedrooms: number;
+  Bathrooms: number;
+  InternalLettingStatus?: string;
+  InternalSaleStatus?: string;
+  TimeAmended: Date;
+}
+
+interface Image {
+  Filepath: string;
+}
