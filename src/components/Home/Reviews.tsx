d8673d91bfdefd0d80c6cc84706596c8258992fc
Jack Coombs <jack.coombs@hotmail.co.uk>
2022-12-24 13:48:55 +0000

    Removed unnecessary import.

--- src/components/Home/Reviews.tsx
+++ src/components/Home/Reviews.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import type { Swiper as SwiperType } from "swiper/types";
-import { Controller } from "swiper";
 import Review from "./Review";
 import ReviewNavBtn from "./ReviewNavBtn";
 
