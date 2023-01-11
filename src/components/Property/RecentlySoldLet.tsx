4868ccbf2e1db4246666088dc129985e4cd7f41f
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-01-11 14:23:12 +0000

    Removed fetch in component. Added properties as props instead.

--- src/components/Property/RecentlySoldLet.tsx
+++ src/components/Property/RecentlySoldLet.tsx
@@ -8,9 +8,15 @@ import SoldLetToggle from "./SoldLetToggle";
 import type { Swiper as SwiperType } from "swiper/types";
 import type { Property } from "../../env";
 
-export default function RecentlySoldLet() {
-  const [recentlySold, setRecentlySold] = useState<Property[]>([]);
-  const [recentlyLet, setRecentlyLet] = useState<Property[]>([]);
+interface Props {
+  salesProperties: Property[];
+  lettingsProperties: Property[];
+}
+
+export default function RecentlySoldLet({
+  salesProperties,
+  lettingsProperties,
+}: Props) {
   const [isStartOrEnd, setIsStartOrEnd] = useState("start");
   const [toggle, setToggle] = useState("SOLD");
 
@@ -20,73 +26,58 @@ export default function RecentlySoldLet() {
     else setIsStartOrEnd("");
   };
 
-  useEffect(() => {
-    const fetchProperties = async (
-      category: string,
-      setState: React.Dispatch<React.SetStateAction<Property[]>>
-    ) => {
-      const res = await fetch(
-        `https://jacobs-server.onrender.com/properties/${category}`
-      );
-      const data = await res.json();
-      setState(
-        data
-          .filter(
-            (property: Property) =>
-              property.InternalSaleStatus !== "For Sale - Available" &&
-              property.InternalLettingStatus !== "To Let - Available"
-          )
-          .sort(function (a: Property, b: Property) {
-            return (
-              (new Date(b.TimeAmended) as any) -
-              (new Date(a.TimeAmended) as any)
-            );
-          })
-      );
-    };
-    fetchProperties("sales", setRecentlySold);
-    fetchProperties("lettings", setRecentlyLet);
-  }, []);
+  const properties = toggle === "SOLD" ? salesProperties : lettingsProperties;
 
   return (
-    <>
-      {recentlySold.length > 0 && (
-        <Swiper
-          onLock={() => setIsStartOrEnd("lock")}
-          onUnlock={() => setIsStartOrEnd("start")}
-          slidesPerView={1.1}
-          spaceBetween={30}
-          onSlideChange={handleSlideChange}
-          breakpoints={{
-            768: {
-              slidesPerView: 2,
-            },
-            1280: {
-              slidesPerView: 3,
-              spaceBetween: 50,
-            },
-          }}
-          className="md:16 overflow-really-visible-mobile flex flex-col gap-8"
-          onSwiper={handleSlideChange}
-        >
-          <div slot="container-start" className="flex justify-between">
-            <h3 className="flex items-center gap-1 text-2xl font-semibold text-primary-100 md:gap-6 lg:text-3xl">
-              Recently {<SoldLetToggle {...{ toggle, setToggle }} />}
-              <p className="hidden md:inline">Properties</p>
-            </h3>
-            <SwiperNavBtns isStartOrEnd={isStartOrEnd} />
-          </div>
-          {[...(toggle === "SOLD" ? recentlySold : recentlyLet)].map(
-            ({ Address1, Address2, ID, PriceString }: Property, index) => (
-              <SwiperSlide key={index}>
-                <PropertyCard
-                  {...{ toggle, Address1, Address2, ID, PriceString }}
-                />
-              </SwiperSlide>
-            )
-          )}
-        </Swiper>
+    <Swiper
+      onLock={() => setIsStartOrEnd("lock")}
+      onUnlock={() => setIsStartOrEnd("start")}
+      slidesPerView={properties.length > 1 ? 1.1 : 1}
+      spaceBetween={30}
+      onSlideChange={handleSlideChange}
+      breakpoints={{
+        768: {
+          slidesPerView: 2,
+        },
+        1280: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      }}
+      className="md:16 overflow-really-visible-mobile flex flex-col gap-8"
+      onSwiper={handleSlideChange}
+    >
+      <div slot="container-start" className="flex justify-between">
+        <h3 className="flex items-center gap-1 text-2xl font-semibold text-primary-100 md:gap-6 lg:text-3xl">
+          Recently {<SoldLetToggle {...{ toggle, setToggle }} />}
+          <p className="hidden md:inline">Properties</p>
+        </h3>
+        <SwiperNavBtns isStartOrEnd={isStartOrEnd} />
+      </div>
+      {properties.map(
+        (
+          {
+            Address1,
+            Address2,
+            ID,
+            PriceString,
+            InternalSaleStatus,
+            InternalLettingStatus,
+          }: Property,
+          index
+        ) => (
+          <SwiperSlide key={index}>
+            <PropertyCard
+              status={
+                InternalLettingStatus
+                  ? InternalLettingStatus
+                  : InternalSaleStatus
+              }
+              {...{ Address1, Address2, ID, PriceString }}
+            />
+          </SwiperSlide>
+        )
       )}
-    </>
+    </Swiper>
   );
 }
