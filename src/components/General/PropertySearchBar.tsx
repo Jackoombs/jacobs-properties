b5672fbe22c1da75766a45f7b8e8fbf0ff93b637
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-06 12:32:36 +0000

    Updated search bar to use react hook form and session storage.

--- src/components/General/PropertySearchBar.tsx
+++ src/components/General/PropertySearchBar.tsx
@@ -1,34 +1,64 @@
 import clsx from "clsx";
-import { useState } from "react";
-import BuyRentToggle from "./BuyRentToggle";
+import BuyRentToggle from "../Form/ReactHook/BuyRentToggle";
+import { useForm, Controller, FormProvider } from "react-hook-form";
+import Error from "../Form/ReactHook/Error";
+import { IoMdSearch } from "react-icons/io";
 
 export default function PropertySearchBar() {
-  const [isFocused, setIsFocused] = useState(false);
-  const [toggle, setToggle] = useState("BUY");
+  const methods = useForm();
+  const { control, handleSubmit, register } = methods;
+
+  const onSubmit = (data: any) => {
+    window.sessionStorage.setItem("location", data.location);
+    window.sessionStorage.setItem("buyOrRent", data.buyOrRent);
+    window.location.href = "/property-search";
+  };
 
   return (
-    <div className="flex w-full flex-col items-center gap-[18px] lg:flex-row">
-      <BuyRentToggle {...{ toggle, setToggle }} />
+    <FormProvider {...methods}>
       <form
-        action=""
-        className={clsx(
-          "box-border flex h-[4.875rem] w-full flex-1 items-center gap-2 rounded-big border-primary-100 bg-white px-[5%] md:px-8",
-          isFocused ? "outline outline-2 outline-primary-100" : ""
-        )}
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-full max-w-4xl flex-col items-center justify-between gap-4 md:flex-row"
       >
-        <input
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          className=" w-full bg-transparent text-base font-medium text-primary-100 placeholder:text-primary-100 focus:outline-none"
-          type="text"
-          placeholder="Begin your search by town or postcode..."
-        />
-        <button type="submit" className="block py-4">
-          <div className="-rotate-45 ">
-            <span className="text-2xl font-bold text-primary-100">&#9906;</span>
+        <div>
+          <Controller
+            render={({ field: { value, onChange, name } }) => (
+              <BuyRentToggle
+                onChange={onChange}
+                value={value}
+                name={name}
+                size="lg"
+                variant="secondary"
+              />
+            )}
+            name="buyOrRent"
+            control={control}
+            defaultValue={"buy"}
+          />
+        </div>
+        <div className="flex w-full flex-col gap-2 text-left">
+          <label
+            className="text-primary-100, hidden font-harm text-xs"
+            htmlFor="location"
+          >
+            Location
+          </label>
+          <div className="relative flex w-full flex-col items-center gap-1">
+            <input
+              {...register("location")}
+              type="text"
+              className={clsx(
+                "flex h-16 w-full items-center rounded-big border-primary-100 bg-white px-6 font-medium text-primary-100 outline-1 placeholder:text-primary-100 focus:outline focus:outline-primary-100"
+              )}
+              placeholder="Begin your search by town or postcode… i.e RG21"
+            />
+            <button className="absolute right-0 ">
+              <IoMdSearch className="box-content p-5 text-2xl text-primary-100" />
+            </button>
+            <Error name="location" />
           </div>
-        </button>
+        </div>
       </form>
-    </div>
+    </FormProvider>
   );
 }
