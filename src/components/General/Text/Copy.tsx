6d14c2cdd535ad9b0e5c7e87cd2ab5fb140bd7d2
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-08 17:01:45 +0000

    Changed medium copy size.

--- src/components/General/Text/Copy.tsx
+++ src/components/General/Text/Copy.tsx
@@ -22,7 +22,7 @@ export default function Copy({
         className,
         textColor,
         size === "sm" && "text-[0.75rem]",
-        size === "md" && "text-[1rem] lg:text-[1.15rem] xl:text-[1.25rem]",
+        size === "md" && "text-[1rem] lg:text-[1.15rem]",
         size === "lg" && "text-[1.125rem] lg:text-[1.2rem] xl:text-[1.375rem]",
         padding === "hero-md" && "pb-6 lg:pb-7",
         padding === "hero-lg" && "pb-6 lg:pb-11"
