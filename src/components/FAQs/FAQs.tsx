4e17eece22df35560700a877c89e9b8298de141e
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-08 16:58:59 +0000

    Updated page copy text and added correct links.

--- src/components/FAQs/FAQs.tsx
+++ src/components/FAQs/FAQs.tsx
@@ -7,6 +7,7 @@ interface Props {
   bgColor?: string;
   client: Client;
   maxFAQs?: number;
+  showLink?: boolean;
 }
 
 export default function FAQs({ bgColor, client, maxFAQs }: Props) {
@@ -15,7 +16,7 @@ export default function FAQs({ bgColor, client, maxFAQs }: Props) {
   const FAQTemplate = FAQJson[client];
 
   return (
-    <ul className="flex flex-col items-center gap-2 md:gap-5">
+    <ul className="flex w-full flex-col items-center gap-2 md:gap-5">
       {FAQTemplate.slice(0, maxFAQs).map(({ question, answer }, index) => (
         <FAQCard
           key={index}
--- src/components/General/ValuationLink.tsx
+++ src/components/General/ValuationLink.tsx
@@ -2,41 +2,74 @@ import SectionSubHeader from "./Text/SectionSubHeader";
 import { FiArrowRight } from "react-icons/fi/index.js";
 import { motion } from "framer-motion";
 import { useState } from "react";
+import FormModal from "../Form/ReactHook/FormModal";
+import InstantForm from "../Form/InstantValuation/Form";
+import ExpertForm from "../Form/ExpertValuation/Form";
 import clsx from "clsx";
 
 interface Props {
   className?: string;
   children: string;
   backgroundColor: "bg-white" | "bg-primary-100" | "bg-secondary-100";
-  link: string;
+  valuationType: "instant" | "expert";
 }
 
 export default function ValuationLink({
   children,
   backgroundColor,
-  link,
   className,
+  valuationType,
 }: Props) {
   const [isHover, setIsHover] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <a
-      className={clsx("w-full rounded-big text-primary-100", backgroundColor)}
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
-      href={link}
-    >
-      <div className="flex flex-col items-start justify-between gap-3 p-6 md:flex-row md:items-center lg:px-12 lg:py-16 xl:py-20">
-        <SectionSubHeader className={className} size="lg">
-          {children}
-        </SectionSubHeader>
-        <motion.div
-          className="relative md:left-[10px]"
-          animate={{ x: isHover ? 10 : 0 }}
+    <>
+      {" "}
+      <button
+        className={clsx("w-full rounded-big text-primary-100", backgroundColor)}
+        onClick={() => setIsOpen(true)}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <div className="flex flex-col items-start justify-between gap-3 p-6 md:flex-row md:items-center lg:px-12 lg:py-16 xl:py-20">
+          <SectionSubHeader className={className} size="lg">
+            {children}
+          </SectionSubHeader>
+          <motion.div
+            className="relative md:left-[10px]"
+            animate={{ x: isHover ? 10 : 0 }}
+          >
+            <FiArrowRight className="text-5xl lg:text-6xl" />
+          </motion.div>
+        </div>
+      </button>
+      {valuationType === "instant" && (
+        <FormModal
+          {...{ isOpen, setIsOpen }}
+          label="Instant valuation"
+          heading={{ text: "Quick and easy valuation", width: "max-w-[34rem]" }}
+          copy={{
+            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,sed diam nonumy eirmod tempor invidunt ut.",
+            width: "max-w-md",
+          }}
         >
-          <FiArrowRight className="text-5xl lg:text-6xl" />
-        </motion.div>
-      </div>
-    </a>
+          <InstantForm {...{ setIsOpen }} />
+        </FormModal>
+      )}
+      {valuationType === "expert" && (
+        <FormModal
+          {...{ isOpen, setIsOpen }}
+          label="Book a viewing"
+          heading={{ text: "Face to face Valuation", width: "max-w-[34rem]" }}
+          copy={{
+            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,sed diam nonumy eirmod tempor invidunt ut.",
+            width: "max-w-md",
+          }}
+        >
+          <ExpertForm {...{ setIsOpen }} />
+        </FormModal>
+      )}
+    </>
   );
 }
