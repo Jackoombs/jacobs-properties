3352482128f6a333c6aef38915f18dfed596d863
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-09 09:32:55 +0000

    Added a sort button to the property search page.

--- src/components/Form/SortInput.tsx
+++ src/components/Form/SortInput.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import type { SetStateAction } from "react";
 import { FaChevronDown } from "react-icons/fa/index.js";
 
@@ -5,19 +6,31 @@ interface Props {
   options: string[];
   label: string;
   name: string;
-  setState: React.Dispatch<SetStateAction<string>>;
+  setState: React.Dispatch<SetStateAction<any>>;
+  className?: string;
 }
 
-export default function SortInput({ options, label, name, setState }: Props) {
+export default function SortInput({
+  options,
+  label,
+  name,
+  setState,
+  className,
+}: Props) {
   return (
-    <div className="flex items-center gap-4 pb-6 text-base text-primary-100">
-      <label className="font-semibold" htmlFor={name}>
+    <div
+      className={clsx(
+        "flex items-center gap-4 text-base text-primary-100",
+        className
+      )}
+    >
+      <label className="cursor-pointer font-semibold" htmlFor={name}>
         {label}
       </label>
       <div className="relative flex items-center">
         <select
           onChange={(e) => setState(e.currentTarget.value)}
-          className="w-40 appearance-none bg-white font-normal"
+          className="w-40 cursor-pointer appearance-none bg-white font-normal"
           id={name}
         >
           {options.map((option) => (
--- src/components/Property/PropertyViewSection.tsx
+++ src/components/Property/PropertyViewSection.tsx
@@ -6,6 +6,8 @@ import PropertyMap from "./PropertyMap";
 import PropertyMapCard from "./PropertyMapCard";
 import { useState } from "react";
 import type { Property } from "../../env";
+import { getNumberFromPriceString } from "../../utils";
+import SortInput from "../Form/SortInput";
 
 interface Props {
   center: google.maps.LatLng | google.maps.LatLngLiteral | undefined;
@@ -24,31 +26,65 @@ export default function PropertyViewSection({
   const [currMapCardLocation, setCurrMapCardLocation] = useState<
     google.maps.LatLng | google.maps.LatLngLiteral | undefined
   >(undefined);
+  const [sortBy, setSortBy] = useState<
+    | "Relevant"
+    | "Address (A-Z)"
+    | "Address (Z-A)"
+    | "Price (Low-high)"
+    | "Price (High-low)"
+  >("Relevant");
 
   const currentMapProperty = properties.find(
     (property) => property.Location === currMapCardLocation
   );
 
+  const sortedData = [...properties].sort((a, b): any => {
+    const numberA = getNumberFromPriceString(a.PriceString);
+    const numberB = getNumberFromPriceString(b.PriceString);
+    if (!numberA || !numberB) return;
+    if (sortBy === "Price (Low-high)") {
+      return numberA - numberB;
+    } else if (sortBy === "Address (A-Z)") {
+      return a.Address1.localeCompare(b.Address1);
+    } else if (sortBy === "Address (Z-A)") {
+      return b.Address1.localeCompare(a.Address1);
+    } else if (sortBy === "Price (High-low)") {
+      return numberB - numberA;
+    } else return properties;
+  });
+
   return (
     <section className="overflow-hidden py-10 md:pb-28">
-      <div className="mx-auto flex w-full max-w-container-lg items-center justify-between pb-8 lg:pb-10">
-        <Copy
-          className="order-1 lg:order-none"
-          size="lg"
-        >{`${properties.length} properties`}</Copy>
+      <div className="mx-auto grid w-full max-w-container-lg grid-cols-2 grid-rows-2 items-center justify-between pb-4 lg:flex lg:gap-36 lg:pb-10">
+        <Copy className="order-1 text-right lg:order-none" size="lg">{`${
+          properties.length
+        } ${properties.length === 1 ? "property" : "properties"}`}</Copy>
+        <SortInput
+          name="sortOptions"
+          label="Sort by: "
+          options={[
+            "Relevant",
+            "Address (A-Z)",
+            "Address (Z-A)",
+            "Price (Low-high)",
+            "Price (High-low)",
+          ]}
+          setState={setSortBy}
+          className="col-span-2 pb-4 lg:flex-1 lg:pb-0"
+        />
         <ViewToggle
           state={viewType}
           setState={setViewType}
           isMobile={isMobile}
         />
       </div>
-      {viewType === "grid" && <PropertyGridView {...{ properties }} />}
-      {viewType === "list" && <PropertyListView {...{ properties }} />}
+      {viewType === "grid" && <PropertyGridView properties={sortedData} />}
+      {viewType === "list" && <PropertyListView properties={sortedData} />}
       {viewType === "map" && (
         <div className="mx-auto max-w-container-lg">
           <PropertyMap
             zoom={12}
-            markers={properties.map((property) => property.Location)}
+            markers={sortedData.map((property) => property.Location)}
             location={center}
             state={currMapCardLocation}
             setState={setCurrMapCardLocation}
