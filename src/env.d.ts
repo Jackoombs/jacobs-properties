11b8cfbb61d024ac850fefd75a9fed797d7ef665
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-27 18:55:34 +0000

    Added new property type.

--- src/env.d.ts
+++ src/env.d.ts
@@ -19,6 +19,82 @@ export interface Property {
   VirtualTour?: string;
 }
 
+export interface Property2 {
+  id: string;
+  type: "selling" | "letting" | "sellingAndLetting";
+  status:
+    | "preAppraisal"
+    | "valuation"
+    | "paidValuation"
+    | "forSale"
+    | "forSaleUnavailable"
+    | "underOffer"
+    | "underOfferUnavailable"
+    | "reserved"
+    | "exchanged"
+    | "completed"
+    | "soldExternally"
+    | "withdrawn"
+    | "valuation"
+    | "toLet"
+    | "toLetUnavailable"
+    | "underOffer"
+    | "underOfferUnavailable"
+    | "arrangingTenancyUnavailable"
+    | "arrangingTenancy"
+    | "tenancyCurrentUnavailable"
+    | "tenancyCurrent"
+    | "tenancyFinished"
+    | "tenancyCancelled"
+    | "sold"
+    | "letByOtherAgent"
+    | "letPrivately"
+    | "provisional"
+    | "withdrawn";
+  description?: string;
+  address1: string;
+  address2: string;
+  postcode: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  receptions: number;
+  images: Image2[];
+  floorplan: Image2[];
+  epc: Image2[];
+  created: string;
+  virtualTour?: string;
+  brochure?: string;
+  location?: google.maps.LatLng | google.maps.LatLngLiteral | undefined;
+}
+
+interface Image2 {
+  readonly _links?: {
+    [name: string]: {
+      href?: string;
+    };
+  };
+  readonly _embedded?: {
+    [name: string]: any;
+  };
+  id: string;
+  created: string; // date-time
+  modified: string; // date-time
+  propertyId: string;
+  url: string;
+  caption?: string;
+  type: "photograph" | "floorplan" | "epc";
+  order?: number; // int32
+  readonly _eTag?: string;
+}
+
+interface Room {
+  name: string;
+  dimensions?: string;
+  dimensionsAlt?: string;
+  description?: string;
+}
+
 interface Image {
   Filepath: string;
   Caption: string;
