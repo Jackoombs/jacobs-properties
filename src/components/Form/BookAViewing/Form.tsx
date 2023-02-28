39d56c4aa7862c80bb0d2475ff4a551876c2b7bd
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-28 10:30:44 +0000

    Fixed default address field not sending.

--- src/components/Form/BookAViewing/Form.tsx
+++ src/components/Form/BookAViewing/Form.tsx
@@ -23,6 +23,7 @@ export default function Form({ price, address, setIsOpen }: Props) {
     mode: "all",
     defaultValues: {
       dates: [{ date: "" }],
+      address,
     },
   });
   const { handleSubmit, reset, watch } = methods;
