e9903358c8ba8ea4645f82743232b6ab0b15578b
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-28 21:54:19 +0000

    Added color variant.

--- src/components/ImageCarousel/ImageSliderNavBtn.tsx
+++ src/components/ImageCarousel/ImageSliderNavBtn.tsx
@@ -5,9 +5,14 @@ import type { Swiper as SwiperType } from "swiper/types";
 interface Props {
   swiper: SwiperType | null;
   isReverse: boolean;
+  variant?: "white" | "primary";
 }
 
-export default function ImageSliderNavBtn({ swiper, isReverse }: Props) {
+export default function ImageSliderNavBtn({
+  swiper,
+  isReverse,
+  variant = "white",
+}: Props) {
   const handleClick = () => {
     isReverse ? swiper?.slidePrev() : swiper?.slideNext();
   };
@@ -16,11 +21,19 @@ export default function ImageSliderNavBtn({ swiper, isReverse }: Props) {
     <button
       onClick={handleClick}
       className={clsx(
-        "absolute z-10 flex h-8 w-8 scale-75 items-center justify-center rounded-full bg-white duration-100 hover:bg-secondary-100 md:scale-100",
-        isReverse ? "left-2 rotate-180 lg:left-7" : "right-2 lg:right-7"
+        "absolute z-10 flex h-8 w-8 scale-75 items-center justify-center rounded-full  duration-100 hover:bg-secondary-100 md:scale-100",
+        isReverse ? "left-2 rotate-180 lg:left-7" : "right-2 lg:right-7",
+        variant === "white" && "bg-white",
+        variant === "primary" && "bg-primary-100"
       )}
     >
-      <IoArrowForwardOutline className="text-lg text-primary-100" />
+      <IoArrowForwardOutline
+        className={clsx(
+          "text-lg",
+          variant === "white" && "text-primary-100",
+          variant === "primary" && "text-white"
+        )}
+      />
     </button>
   );
 }
