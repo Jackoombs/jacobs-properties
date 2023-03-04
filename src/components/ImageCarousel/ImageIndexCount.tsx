0e006afd94ba3b90b153232c7321c724f9e32b90
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-03-04 00:04:28 +0000

    Small style change.

--- src/components/ImageCarousel/ImageIndexCount.tsx
+++ src/components/ImageCarousel/ImageIndexCount.tsx
@@ -11,7 +11,7 @@ export default function ImageIndexCount({
   numberOfImages,
 }: Props) {
   return (
-    <div className="absolute bottom-2 right-2 z-10 flex h-8 w-20 items-center justify-center gap-2 rounded bg-white font-medium text-primary-100 md:bottom-6 md:right-6 md:w-24">
+    <div className="absolute bottom-2 right-2 z-10 flex h-8 w-24 items-center justify-between rounded bg-white px-3 font-medium text-primary-100 md:bottom-6 md:right-6">
       <BsFillCameraFill className="text-lg text-secondary-100" />
       <p className="text-sm tracking-[2.8px]">{`${
         currentIndex + 1
