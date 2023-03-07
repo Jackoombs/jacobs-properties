b23245f2109f09ef663d1e895d3ee65e532ddf7e
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-03-07 12:54:09 +0000

    Added trunate function.

--- src/utils.tsx
+++ src/utils.tsx
@@ -120,3 +120,11 @@ export const formatDates = (data: Data) => {
   });
   return { ...data, dates: formatedData };
 };
+
+export const truncateDescription = (description: string, length: number) => {
+  const words = description.split(" ");
+  if (words.length > length) {
+    return words.slice(0, length).join(" ") + "...";
+  }
+  return description;
+};
