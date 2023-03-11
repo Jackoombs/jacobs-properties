5eeee17994ab29a4abf7f1be993d8694ef863005
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-03-11 13:44:49 +0000

    Border style change.

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -20,7 +20,7 @@ export default function Input({
   return (
     <div className="flex flex-col gap-1 font-harm">
       <input
-        className="flex h-16 w-full items-center rounded-big px-5 text-primary-100 outline-none outline-offset-0 placeholder:text-placeholder focus:outline-primary-100"
+        className="flex h-16 w-full items-center rounded-big px-5 text-primary-100 outline-none outline-offset-0 placeholder:text-placeholder focus:outline-1 focus:outline-primary-100"
         {...register(id)}
         {...{ type, placeholder }}
       />
