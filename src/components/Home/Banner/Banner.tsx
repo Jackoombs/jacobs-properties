55fa4dcb00ab0d85cd0b64565128b39c4cab2ccf
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-03-12 23:02:07 +0000

    Added new image. Changed speed slightly.

--- src/components/Home/Banner/Banner.tsx
+++ src/components/Home/Banner/Banner.tsx
@@ -2,6 +2,7 @@ import Background from "../../../assets/images/home/background.webp";
 import Background2 from "../../../assets/images/home/background_2.webp";
 import Background3 from "../../../assets/images/home/background_3.webp";
 import Background4 from "../../../assets/images/home/background_4.webp";
+import Background5 from "../../../assets/images/home/background_5.webp";
 import InstantValuationButton from "../../Form/InstantValuation/Button";
 import ExpertValuationButton from "../../Form/ExpertValuation/Button";
 import Link from "../../General/Link";
@@ -38,24 +39,28 @@ export default function Banner() {
       backgroundSrc: Background4,
       objectFit: "78% 0",
     },
+    {
+      backgroundSrc: Background5,
+      objectFit: "76% 0",
+    },
   ];
 
   return (
     <section className="top-0 -z-10 grid h-[100svh] w-screen grid-rows-[1fr_auto_auto] lg:grid-rows-[1fr_auto_1fr]">
       <div className="row-start-2 mx-auto w-full max-w-container-lg self-center pb-11 lg:pb-0">
-        <div className="mx-auto flex flex-col text-white md:mx-0 lg:max-w-[44rem]">
+        <div className="mx-auto flex flex-col text-white md:mx-0 md:max-w-[40rem] xl:max-w-[48rem]">
           <PageHeader textColor="text-white" padding="hero">
-            A people-first estate agent in Basingstoke
+            A people-first estate agent in Basingstoke & Surrounding Villages
           </PageHeader>
           <Copy
             size="lg"
             textColor="text-white"
-            className="pb-8 md:max-w-lg"
+            className="pb-8 md:max-w-xl"
             padding="hero-lg"
           >
-            Jacobs Properties is a family-run estate and letting agency in
-            Basingstoke, delivering exceptional results and providing solutions
-            for all things property.
+            Jacobs Properties is a independent family-run estate and letting
+            agency in Basingstoke, delivering exceptional results and providing
+            solutions for all things property.
           </Copy>
           <div className="flex flex-col items-center justify-center gap-3 md:flex-row md:justify-start">
             <InstantValuationButton size="lg" variant="white">
@@ -72,13 +77,13 @@ export default function Banner() {
         swiper={swiper}
         imageLength={backGrounds.length}
       />
-      <div className="absolute top-0  right-0 -z-10 h-[100svh] w-screen">
+      <div className="absolute top-0 right-0 -z-10 h-[100svh] w-screen">
         <Swiper
           onSwiper={(e) => setSwiper(e)}
           onSlideChange={(e) => setActiveIndex(e.activeIndex)}
           modules={[Autoplay, EffectFade]}
           autoplay={{ delay: 3000 }}
-          speed={1400}
+          speed={2000}
           navigation
           pagination={{ clickable: true, dynamicBullets: true }}
           effect="fade"
